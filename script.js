e787854fd09b1c078a9ba9e2d5f66a566c412a09
Sam <samwong59@outlook.com>
2022-12-20 12:44:19 +0000

    Create operate function
    Takes two numbers and an operator and calls and returns the appropiate
    function. e.g. operate(1, 2, "+") will call add(1, 2) and return 3

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(NUM_num1, NUM_num2) {
 function divide(NUM_num1, NUM_num2) {
   return NUM_num1 / NUM_num2;
 }
+
+function operate(NUM_num1, NUM_num2, STR_operator) {
+  switch(STR_operator) {
+    case "+":
+      return add(NUM_num1, NUM_num2);
+    case "-":
+      return subtract(NUM_num1, NUM_num2);
+    case "*":
+      return multiply(NUM_num1, NUM_num2);
+    case "/":
+      return divide(NUM_num1, NUM_num2);
+    default:
+      return "Invalid input!";
+  }
+}
