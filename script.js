fdf242906d2c7b492cd59a7faec9d5e58607a429
Sam <samwong59@outlook.com>
2022-12-20 12:41:36 +0000

    Create divide function

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ function subtract(NUM_num1, NUM_num2) {
 function multiply(NUM_num1, NUM_num2) {
   return NUM_num1 * NUM_num2;
 }
+
+function divide(NUM_num1, NUM_num2) {
+  return NUM_num1 / NUM_num2;
+}
