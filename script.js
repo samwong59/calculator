19823b1f19b0efb868e4f25b7af3f4b7e8a22f82
Sam <samwong59@outlook.com>
2022-12-20 12:39:30 +0000

    Create multiply function

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function add(NUM_num1, NUM_num2) {
 function subtract(NUM_num1, NUM_num2) {
   return NUM_num1 - NUM_num2;
 }
+
+function multiply(NUM_num1, NUM_num2) {
+  return NUM_num1 * NUM_num2;
+}
